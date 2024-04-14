c9e2f9866812a3adf9ce5e94d196a2efc0a0ad7c
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-14 17:51:43 +0530

    completed mobileNavbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "@/components/MoblieNav";
 
 const Navbar = () => {
-  return <div>Hi there</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Voom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Voom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* TODO:Clerk - User Management */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
