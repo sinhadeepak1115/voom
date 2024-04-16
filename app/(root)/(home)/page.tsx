b13e8a78e07a1104cfdeed72fe77e22776ae13ff
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-16 20:48:21 +0530

    checkpoint

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
 import React from "react";
 
 const Home = () => {
@@ -23,6 +24,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <MeetingTypeList />
     </section>
   );
 };
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import HomeCard from "./HomeCard";
+
+const MeetingTypeList = () => {
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handelClick={() => setMeetingState("isInstantMeeting")}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        className="bg-blue-1"
+        handelClick={() => setMeetingState("isJoiningMeeting")}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handelClick={() => setMeetingState("isScheduleMeeting")}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-yellow-1"
+        handelClick={() => router.push("/recordings")}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
