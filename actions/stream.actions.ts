6a638306ba5eac8a387e659d0939ed71a54f8926
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-19 23:45:15 +0530

    makeing api token for videocall

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,8 +1,17 @@
-"use server"
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
 
 export const tokenProvider = async () => {
-  const user = await 
+  const user = await currentUser();
+
+  if (!user) throw new Error("User is not Logged in");
+  if (!apiKey) throw new Error("No API Key");
+  if (!apiSecret) throw new Error("No API secret");
+
+  const client = new StreamClient(apiKey, apiSecret);
 };
