672f84487c5fd6c20b6baacacee5eb773e0f8661
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-23 23:53:19 +0530

    checkpoint

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,15 +1,44 @@
 "use client";
+
 import React from "react";
 import { useState } from "react";
 import HomeCard from "./HomeCard";
 import MeetingModal from "./MeetingModal";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 
-const createMeeting = () => {};
+const initialValues = {
+  dateTime: new Date(),
+  description: "",
+  link: "",
+};
 
 const MeetingTypeList = () => {
+  const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
-  >();
+  >(undefined);
+
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState(initialValues);
+  const [callDetails, setCallDetails] = useState<Call>();
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
