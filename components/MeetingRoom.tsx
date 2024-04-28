b9b544010d4b42ffaf34bb4d5f684d0ca5712d01
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-28 19:26:43 +0530

    adding Dropdown menu

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -7,6 +7,14 @@ import {
 import React from "react";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
@@ -40,6 +48,17 @@ const MeetingRoom = () => {
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls />
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </section>
   );
