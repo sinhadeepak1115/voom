3807eadd82832f9d89f7fc5062cb8c2efa0816da
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-05-07 00:11:49 +0530

    checkpoint w

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
 
 import Navbar from "@/components/Navbar";
 
@@ -14,6 +15,7 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
       <Navbar />
 
       <div className="flex">
+        <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
