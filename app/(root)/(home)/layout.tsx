dc3c86a031d4a6619e2c1a24c70e4a8d94f9ab9b
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-05-07 00:05:51 +0530

    checkpoint .

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 import Navbar from "@/components/Navbar";
-import Sidebar from "../../../components/Sidebar";
+
 export const metadata: Metadata = {
   title: "YOOM",
   description: "A workspace for your team, powered by Stream Chat and Clerk.",
@@ -14,8 +14,6 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
       <Navbar />
 
       <div className="flex">
-        <Sidebar />
-
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
